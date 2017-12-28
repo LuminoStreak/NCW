178f27969741573531b703065f580ec78cf51b5f
vikneshraj <vikneshrajsp@outlook.com>
2017-12-28 15:57:39 +0530

    Added sample EF class

--- CoreDal/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CoreDal
-{
-    public class Class1
-    {
-    }
-}
---
+++ CoreDal/SampleEntity.cs
@@ -0,0 +1,10 @@
+namespace CoreDal 
+{  
+    public class SampleEntity : BaseEntity  
+    {  
+        public string FirstName { get; set; }  
+        public string LastName { get; set; }  
+        public string Email { get; set; }  
+        public string EnrollmentNo { get; set; }  
+    }
+}
